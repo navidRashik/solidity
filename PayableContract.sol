d2e474b24f80b182fd02adb16962941b50127747
navidRashik <navid.rashik@gmail.com>
2017-09-19 14:58:42 +0600

    Create PayableContract.sol

---
+++ PayableContract.sol
@@ -0,0 +1,13 @@
+pragma solidity ^0.4.0;
+
+contract PayableContract{
+    function PayableContract() {
+        
+    }
+    function recieveFunds() payable{
+        
+    }
+    function getter() constant returns(uint){
+        return this.balance ; 
+    }
+}
