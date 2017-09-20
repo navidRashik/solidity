c862b68ab29a607325c80d70d03cb4856137a75b
navidRashik <navid.rashik@gmail.com>
2017-09-20 19:39:13 +0600

    Update CustodialContract.sol

--- CustodialContract.sol
+++ CustodialContract.sol
@@ -3,6 +3,8 @@ pragma solidity ^0.4.0;
 contract CustodialContract{
     address client;
     bool public switchFunds = false;
+    event depositStatus(string _msg1, address user, string _msg2 , uint _amount);
+    event userStatus(string _msg);
     function CustodialContract(){
         client = msg.sender;
     }
@@ -16,11 +18,13 @@ contract CustodialContract{
     }
     
     function depositFund() payable{
-        
+        depositStatus('User has deposit some money from account : ',msg.sender,
+                        ' and the amount is : ', msg.value);
     }
     
     function withDrawFunds(uint amount) ifClient {
         if(client.send(amount)){
+            userStatus('user withDrawed some money!!!');
             switchFunds = true;
         }
         else{
